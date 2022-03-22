316dcbf52a5cde9890f4ba39ac45c1086688b06d
aph <81857274+aphkyle@users.noreply.github.com>
2022-03-22 09:15:49 +0800

    better condition check

--- download_data.py
+++ download_data.py
@@ -1,12 +1,11 @@
 import pathlib
-import ast
 import zlib
 import urllib.request as request
 
 word_lst = request.urlopen(
     "https://raw.githubusercontent.com/InnovativeInventor/dict4schools/master/safedict_full.txt"
 )
-lst = [word for word in (word_lst.read().decode()).split("\n") if len(word) == 5]
+lst = [word for word in (word_lst.read().decode()).split("\n") if len(word) == 5 and word.isalpha()]
 
 p = pathlib.Path("assets")
 p.mkdir(parents=True, exist_ok=True)
