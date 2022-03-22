66ad2952b7b278d28799e6157b1b4f64d7a96e06
aph <81857274+aphkyle@users.noreply.github.com>
2022-03-22 18:15:09 +0800

    added an evil twin

---
+++ wink/download_data.py
@@ -0,0 +1,15 @@
+import pathlib
+import ast
+import zlib
+import urllib.request as request
+
+word_lst = request.urlopen(
+    "https://raw.githubusercontent.com/InnovativeInventor/dict4schools/master/safedict_full.txt"
+)
+lst = [word for word in (word_lst.read().decode()).split("\n") if len(word) == 5 and word.isalpha()]
+
+p = pathlib.Path("assets")
+p.mkdir(parents=True, exist_ok=True)
+
+with (p / "WORDS_LIST_COMPRESSED").open("wb") as file:
+    file.write(zlib.compress("".join(lst).encode()))
