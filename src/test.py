5f9cabbd3a1177013f74a49c75388d3cc8c1df1b
aph <81857274+aphkyle@users.noreply.github.com>
2022-03-22 20:49:54 +0800

    Delete test.py

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import playsound, pathlib,time
-
-if not pathlib.Path("assets").exists():
-    ASSETS = pathlib.Path("..") / "assets"
-else:
-    ASSETS = pathlib.Path("assets")
-
-playsound.playsound(ASSETS / "BoxCat-Games-Assignment.wav", block=False)
-time.sleep(64)
