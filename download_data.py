fcfad5b75acb758590461855e142fcf2c0473486
aph <81857274+aphkyle@users.noreply.github.com>
2022-03-22 09:10:33 +0800

    fixing the size
    :P

--- download_data.py
+++ download_data.py
@@ -3,10 +3,10 @@ import ast
 import zlib
 import urllib.request as request
 
-word = request.urlopen(
+word_lst = request.urlopen(
     "https://raw.githubusercontent.com/InnovativeInventor/dict4schools/master/safedict_full.txt"
 )
-lst = (word.read().decode()).split("\n")
+lst = [word for word in (word_lst.read().decode()).split("\n") if len(word) == 5]
 
 p = pathlib.Path("assets")
 p.mkdir(parents=True, exist_ok=True)
